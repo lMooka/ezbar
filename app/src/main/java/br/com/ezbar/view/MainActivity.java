f8a5b941dfdc1aec51229ac9d3f3ab9f400ab1e5
Guilherme <guilherme.sdes@gmail.com>
2018-11-09 08:03:14 -0300

    algumas coisas do login

--- app/src/main/java/br/com/ezbar/view/MainActivity.java
+++ app/src/main/java/br/com/ezbar/view/MainActivity.java
@@ -1,66 +1,22 @@
 package br.com.ezbar.view;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.TextView;
 
 import br.com.ezbar.R;
-import br.com.ezbar.model.business.LoginCredentials;
-import br.com.ezbar.model.service.IServiceListener;
-import br.com.ezbar.model.service.LoginService;
-import br.com.ezbar.model.service.Service;
 
-public class MainActivity extends AppCompatActivity implements IServiceListener {
-
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        LoginService ls = new LoginService(this, new LoginCredentials("Guilherme@gmail.com", "123456"));
-        ls.request();
     }
 
     @Override
-    public void requestComplete(Service service) {
-        if(service instanceof LoginService) {
-            // login success
-        }
-    }
-
-    @Override
-    public void requestError(Service service) {
-        if(service instanceof LoginService) {
-            // login fail
-        }
+    protected void onStart() {
+        super.onStart();
+        startActivity(new Intent(this, LoginActivity.class));
     }
 }
